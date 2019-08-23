83446d0914d76e016802eeaa53999fd25ba7f67b
Ricky Hou <1107786871@qq.com>
2019-08-23 10:57:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-#!usr/bin
-pip3 install -r requirements.txt
-rm -rf ./audio
-rm -rf ./pic
-rm -rf ./words
-mkdir audio
-mkdir pic
-mkdir words
-python3 split.py
-python3 scrapy.py
-python3 combain.py
+#!/usr/bin
+path=$(pwd)
+pip3 install -r "$path/requirements.txt"
+rm -rf "$path/audio"
+rm -rf "$path/pic"
+rm -rf "$path/words"
+mkdir "$path/audio"
+mkdir "$path/pic"
+mkdir "$path/words"
+python3 "$path/split.py"
+python3 "$path/scrapy.py"
+python3 "$path/combain.py"
